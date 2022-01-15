3dd6080b1c202613ac1abf64cc420d9472cfc6d3
sniggo jain <snigdha.jain30@gmail.com>
2022-01-15 16:57:21 +0530

    integrate with postgres

--- socialApi/settings.py
+++ socialApi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-f3*l%w3o7!13%t=4p53mjul+d-_l&4xxctwcxf^4+zkw-#a6+%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend-social-api.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -73,13 +74,24 @@ WSGI_APPLICATION = 'socialApi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd38ebqkluqk48o',
+        'USER':'fbhkyczvzkiqdw',
+        'PASSWORD':'30a18c060d5f68827c221f1b8706f11889353bd363052457b139cd2f6a176bbe',
+        'HOST':'ec2-54-158-232-223.compute-1.amazonaws.com',
+        'PORT':'5432'
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -121,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+django_heroku.settings(locals())
